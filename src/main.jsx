d6359508aa3222c2037d0261e62d1ff054eb2bca
tahaeuqd <thaqifzarif.byr0001@edidik.edu.my>
2026-09-05 21:20:06 +0800

    Initialize main.jsx and render NutriXApp

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NutriXApp from "../NutriX.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <NutriXApp />
+  </React.StrictMode>
+);
